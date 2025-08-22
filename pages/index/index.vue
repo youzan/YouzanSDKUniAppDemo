4833b439cf92434af886f7840e7006f27d3446ca
liusaideng <liusaideng@youzan.com>
2025-08-22 18:13:09 +0800

    1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -44,6 +44,7 @@
 </template>
 
 <script>
+	const systemInfo = uni.getSystemInfoSync();
 	var yzModule = uni.requireNativePlugin("YouzanUniPlugin-YZUniModule")
 	export default {
 		onLoad() {
@@ -255,9 +256,11 @@
 			
 			// 初始化SDK
 			initSDK() {
+				// app_key andoid 和 ios 是不一样的
+				const appkey = systemInfo.platform.toLowerCase() == "android" ?'76a4fbe055844b55be81114dddf6fd31': "xxx"
 				const config = {
 					'client_id': '1c3691de6f9aebc0d4',
-					'app_key': '82f6a3c99264402f9d57897ba4d4ca38',
+					'app_key': appkey,
 					'scheme': 'xxxx',
 					"debug":false
 				}
